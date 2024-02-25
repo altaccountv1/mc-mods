3fcf51dc9edb5b2f42ac6b07213a2da279985da4
altaccountv1 <144928943+altaccountv1@users.noreply.github.com>
2024-02-25 19:41:11 +0000

    Update Gui 2.lua

--- Gui 2.lua
+++ Gui 2.lua
@@ -1,36 +1,39 @@
 local Converted = {
-    ["_ScreenGui"] = Instance.new("ScreenGui");
-    ["_Frame"] = Instance.new("Frame");
-    ["_ScrollingFrame"] = Instance.new("ScrollingFrame");
-    ["_Buttons"] = Instance.new("Folder");
-    ["_Sound"] = Instance.new("Folder");
-    ["_SoundTxt"] = Instance.new("TextBox");
-    ["_UICorner"] = Instance.new("UICorner");
-    ["_Play"] = Instance.new("TextButton");
-    ["_UICorner1"] = Instance.new("UICorner");
-    ["_Reload"] = Instance.new("Folder");
-    ["_Reload1"] = Instance.new("TextButton");
-    ["_UICorner2"] = Instance.new("UICorner");
-    ["_Change"] = Instance.new("Folder");
-    ["_Change1"] = Instance.new("TextButton");
-    ["_UICorner3"] = Instance.new("UICorner");
-    ["_Type"] = Instance.new("TextBox");
-    ["_UICorner4"] = Instance.new("UICorner");
-    ["_Style"] = Instance.new("TextBox");
-    ["_UICorner5"] = Instance.new("UICorner");
-    ["_Value"] = Instance.new("TextBox");
-    ["_UICorner6"] = Instance.new("UICorner");
-    ["_Heat"] = Instance.new("Folder");
-    ["_InfHeat"] = Instance.new("TextButton");
-    ["_UICorner7"] = Instance.new("UICorner");
-    ["_Credits"] = Instance.new("TextLabel");
-    ["_UICorner8"] = Instance.new("UICorner");
-    ["_X"] = Instance.new("TextButton");
-    ["_UICorner9"] = Instance.new("UICorner");
-    ["_UICorner10"] = Instance.new("UICorner");
-    ["_Open"] = Instance.new("TextButton");
-    ["_UICorner11"] = Instance.new("UICorner");
-    ["_UIStroke"] = Instance.new("UIStroke");
+	["_ScreenGui"] = Instance.new("ScreenGui");
+	["_Frame"] = Instance.new("Frame");
+	["_ScrollingFrame"] = Instance.new("ScrollingFrame");
+	["_Buttons"] = Instance.new("Folder");
+	["_Sound"] = Instance.new("Folder");
+	["_SoundTxt"] = Instance.new("TextBox");
+	["_UICorner"] = Instance.new("UICorner");
+	["_Play"] = Instance.new("TextButton");
+	["_UICorner1"] = Instance.new("UICorner");
+	["_Reload"] = Instance.new("Folder");
+	["_Reload1"] = Instance.new("TextButton");
+	["_UICorner2"] = Instance.new("UICorner");
+	["_Change"] = Instance.new("Folder");
+	["_Change1"] = Instance.new("TextButton");
+	["_UICorner3"] = Instance.new("UICorner");
+	["_Type"] = Instance.new("TextBox");
+	["_UICorner4"] = Instance.new("UICorner");
+	["_Style"] = Instance.new("TextBox");
+	["_UICorner5"] = Instance.new("UICorner");
+	["_Value"] = Instance.new("TextBox");
+	["_UICorner6"] = Instance.new("UICorner");
+	["_Heat"] = Instance.new("Folder");
+	["_InfHeat"] = Instance.new("TextButton");
+	["_UICorner7"] = Instance.new("UICorner");
+	["_Dragon"] = Instance.new("Folder");
+	["_DragonToggle"] = Instance.new("TextButton");
+	["_UICorner8"] = Instance.new("UICorner");
+	["_Credits"] = Instance.new("TextLabel");
+	["_UICorner9"] = Instance.new("UICorner");
+	["_X"] = Instance.new("TextButton");
+	["_UICorner10"] = Instance.new("UICorner");
+	["_UICorner11"] = Instance.new("UICorner");
+	["_Open"] = Instance.new("TextLabel");
+	["_UICorner12"] = Instance.new("UICorner");
+	["_UIStroke"] = Instance.new("UIStroke");
 }
 
 -- Properties:
@@ -39,7 +42,7 @@ Converted["_ScreenGui"].ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 Converted["_ScreenGui"].Parent = game:GetService("CoreGui")
 
 Converted["_Frame"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_Frame"].BackgroundColor3 = Color3.fromRGB(42, 42, 42)
+Converted["_Frame"].BackgroundColor3 = Color3.fromRGB(42.000001296401024, 42.000001296401024, 42.000001296401024)
 Converted["_Frame"].BackgroundTransparency = 0.25
 Converted["_Frame"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Frame"].BorderSizePixel = 0
@@ -51,7 +54,7 @@ Converted["_ScrollingFrame"].ScrollBarImageColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_ScrollingFrame"].ScrollBarThickness = 10
 Converted["_ScrollingFrame"].Active = true
 Converted["_ScrollingFrame"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_ScrollingFrame"].BackgroundColor3 = Color3.fromRGB(42, 42, 42)
+Converted["_ScrollingFrame"].BackgroundColor3 = Color3.fromRGB(42.000001296401024, 42.000001296401024, 42.000001296401024)
 Converted["_ScrollingFrame"].BackgroundTransparency = 0.25
 Converted["_ScrollingFrame"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_ScrollingFrame"].BorderSizePixel = 0
@@ -73,7 +76,7 @@ Converted["_SoundTxt"].Text = ""
 Converted["_SoundTxt"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_SoundTxt"].TextSize = 14
 Converted["_SoundTxt"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_SoundTxt"].BackgroundColor3 = Color3.fromRGB(75, 147, 255)
+Converted["_SoundTxt"].BackgroundColor3 = Color3.fromRGB(75.00000312924385, 147.00000643730164, 255)
 Converted["_SoundTxt"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_SoundTxt"].BorderSizePixel = 0
 Converted["_SoundTxt"].Position = UDim2.new(0.342777789, 0, 0.0612500012, 0)
@@ -88,7 +91,7 @@ Converted["_Play"].Text = "Play"
 Converted["_Play"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Play"].TextSize = 14
 Converted["_Play"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_Play"].BackgroundColor3 = Color3.fromRGB(75, 138, 255)
+Converted["_Play"].BackgroundColor3 = Color3.fromRGB(75.00000312924385, 138.00000697374344, 255)
 Converted["_Play"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Play"].BorderSizePixel = 0
 Converted["_Play"].Position = UDim2.new(0.798777819, 0, 0.0599999987, 0)
@@ -106,7 +109,7 @@ Converted["_Reload1"].Text = "Reload Character"
 Converted["_Reload1"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Reload1"].TextSize = 14
 Converted["_Reload1"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_Reload1"].BackgroundColor3 = Color3.fromRGB(170, 255, 255)
+Converted["_Reload1"].BackgroundColor3 = Color3.fromRGB(170.0000050663948, 255, 255)
 Converted["_Reload1"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Reload1"].BorderSizePixel = 0
 Converted["_Reload1"].Position = UDim2.new(0.186666667, 0, 0.321298838, 0)
@@ -123,7 +126,7 @@ Converted["_Change1"].Font = Enum.Font.Cartoon
 Converted["_Change1"].Text = "Change Value"
 Converted["_Change1"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Change1"].TextSize = 14
-Converted["_Change1"].BackgroundColor3 = Color3.fromRGB(6, 197, 117)
+Converted["_Change1"].BackgroundColor3 = Color3.fromRGB(6.0000005923211575, 197.00001865625381, 117.00000822544098)
 Converted["_Change1"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Change1"].BorderSizePixel = 0
 Converted["_Change1"].Position = UDim2.new(0.0466666669, 0, 0.119999997, 0)
@@ -136,11 +139,11 @@ Converted["_UICorner3"].Parent = Converted["_Change1"]
 Converted["_Type"].CursorPosition = -1
 Converted["_Type"].Font = Enum.Font.Cartoon
 Converted["_Type"].PlaceholderColor3 = Color3.fromRGB(0, 0, 0)
-Converted["_Type"].PlaceholderText = "Name"
+Converted["_Type"].PlaceholderText = "Type"
 Converted["_Type"].Text = ""
 Converted["_Type"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Type"].TextSize = 14
-Converted["_Type"].BackgroundColor3 = Color3.fromRGB(140, 140, 140)
+Converted["_Type"].BackgroundColor3 = Color3.fromRGB(140.00000685453415, 140.00000685453415, 140.00000685453415)
 Converted["_Type"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Type"].BorderSizePixel = 0
 Converted["_Type"].Position = UDim2.new(1.15873015, 0, 0.600000024, 0)
@@ -157,7 +160,7 @@ Converted["_Style"].PlaceholderText = "Style"
 Converted["_Style"].Text = ""
 Converted["_Style"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Style"].TextSize = 14
-Converted["_Style"].BackgroundColor3 = Color3.fromRGB(140, 140, 140)
+Converted["_Style"].BackgroundColor3 = Color3.fromRGB(140.00000685453415, 140.00000685453415, 140.00000685453415)
 Converted["_Style"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Style"].BorderSizePixel = 0
 Converted["_Style"].Position = UDim2.new(1.15873015, 0, 0, 0)
@@ -174,7 +177,7 @@ Converted["_Value"].PlaceholderText = "Value"
 Converted["_Value"].Text = ""
 Converted["_Value"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Value"].TextSize = 14
-Converted["_Value"].BackgroundColor3 = Color3.fromRGB(76, 100, 206)
+Converted["_Value"].BackgroundColor3 = Color3.fromRGB(76.0000030696392, 100.00000923871994, 206.000018119812)
 Converted["_Value"].BackgroundTransparency = 0.25
 Converted["_Value"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Value"].BorderSizePixel = 0
@@ -193,7 +196,7 @@ Converted["_InfHeat"].Text = "Infinite Heat"
 Converted["_InfHeat"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_InfHeat"].TextSize = 14
 Converted["_InfHeat"].AnchorPoint = Vector2.new(0.5, 0.5)
-Converted["_InfHeat"].BackgroundColor3 = Color3.fromRGB(255, 85, 0)
+Converted["_InfHeat"].BackgroundColor3 = Color3.fromRGB(255, 85.0000025331974, 0)
 Converted["_InfHeat"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_InfHeat"].BorderSizePixel = 0
 Converted["_InfHeat"].Position = UDim2.new(0.48888889, 0, 0.322794199, 0)
@@ -202,6 +205,24 @@ Converted["_InfHeat"].Name = "InfHeat"
 Converted["_InfHeat"].Parent = Converted["_Heat"]
 
 Converted["_UICorner7"].Parent = Converted["_InfHeat"]
+
+Converted["_Dragon"].Name = "Dragon"
+Converted["_Dragon"].Parent = Converted["_Buttons"]
+
+Converted["_DragonToggle"].Font = Enum.Font.Cartoon
+Converted["_DragonToggle"].Text = "Dragon Style"
+Converted["_DragonToggle"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_DragonToggle"].TextSize = 14
+Converted["_DragonToggle"].AnchorPoint = Vector2.new(0.5, 0.5)
+Converted["_DragonToggle"].BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Converted["_DragonToggle"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_DragonToggle"].BorderSizePixel = 0
+Converted["_DragonToggle"].Position = UDim2.new(0.797777772, 0, 0.321985483, 0)
+Converted["_DragonToggle"].Size = UDim2.new(0, 126, 0, 50)
+Converted["_DragonToggle"].Name = "DragonToggle"
+Converted["_DragonToggle"].Parent = Converted["_Dragon"]
+
+Converted["_UICorner8"].Parent = Converted["_DragonToggle"]
 
 Converted["_Credits"].Font = Enum.Font.Gotham
 Converted["_Credits"].Text = "Ducksy's Panel Ultimate"
@@ -216,13 +237,13 @@ Converted["_Credits"].Size = UDim2.new(0, 450, 0, 25)
 Converted["_Credits"].Name = "Credits"
 Converted["_Credits"].Parent = Converted["_Frame"]
 
-Converted["_UICorner8"].Parent = Converted["_Credits"]
+Converted["_UICorner9"].Parent = Converted["_Credits"]
 
 Converted["_X"].Font = Enum.Font.Cartoon
 Converted["_X"].Text = "X"
 Converted["_X"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_X"].TextSize = 20
-Converted["_X"].BackgroundColor3 = Color3.fromRGB(255, 43, 6)
+Converted["_X"].BackgroundColor3 = Color3.fromRGB(255, 43.00000123679638, 6.0000005923211575)
 Converted["_X"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_X"].BorderSizePixel = 0
 Converted["_X"].Position = UDim2.new(0.941999972, 0, 0, 0)
@@ -230,15 +251,15 @@ Converted["_X"].Size = UDim2.new(0, 29, 0, 29)
 Converted["_X"].Name = "X"
 Converted["_X"].Parent = Converted["_Frame"]
 
-Converted["_UICorner9"].Parent = Converted["_X"]
+Converted["_UICorner10"].Parent = Converted["_X"]
 
-Converted["_UICorner10"].Parent = Converted["_Frame"]
+Converted["_UICorner11"].Parent = Converted["_Frame"]
 
 Converted["_Open"].Font = Enum.Font.Cartoon
 Converted["_Open"].Text = "Open"
 Converted["_Open"].TextColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Open"].TextSize = 14
-Converted["_Open"].BackgroundColor3 = Color3.fromRGB(23, 184, 112)
+Converted["_Open"].BackgroundColor3 = Color3.fromRGB(23.000000528991222, 184.00000423192978, 112.0000085234642)
 Converted["_Open"].BorderColor3 = Color3.fromRGB(0, 0, 0)
 Converted["_Open"].BorderSizePixel = 0
 Converted["_Open"].Position = UDim2.new(0.884726822, 0, 0.467741936, 0)
@@ -246,10 +267,10 @@ Converted["_Open"].Size = UDim2.new(0, 98, 0, 31)
 Converted["_Open"].Name = "Open"
 Converted["_Open"].Parent = Converted["_ScreenGui"]
 
-Converted["_UICorner11"].Parent = Converted["_Open"]
+Converted["_UICorner12"].Parent = Converted["_Open"]
 
 Converted["_UIStroke"].ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-Converted["_UIStroke"].Color = Color3.fromRGB(65, 65, 65)
+Converted["_UIStroke"].Color = Color3.fromRGB(65.0000037252903, 65.0000037252903, 65.0000037252903)
 Converted["_UIStroke"].Parent = Converted["_Open"]
 
 local u1 = game.Players.LocalPlayer
@@ -333,7 +354,12 @@ Converted["_Change1"].Activated:Connect(function()
 end)
 
 
-
+Converted["_DragonToggle"].Activated:Connect(function()
+    _G.MorphMod = true
+    _G.VoiceMod = true
+    _G.DEMoveset = false
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/altaccountv1/no-idea/main/Dragon%20New%20No%20obfuscate.lua"))();
+end)
 Converted["_Reload1"].Activated:Connect(function()
     local oldcframe = char.HumanoidRootPart.CFrame
     interf.Client.Disabled = true
